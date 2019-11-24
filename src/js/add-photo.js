dedc350db334a8ba128a84c2060432a464ec2ac7
Saashen <saashen.kyiv@gmail.com>
2019-11-24 15:53:48 +0200

    addPhoto before merge

--- src/js/add-photo.js
+++ src/js/add-photo.js
@@ -1,22 +1,30 @@
 import Dropzone from 'dropzone';
 import 'dropzone/dist/min/dropzone.min.css';
 
-const dropBoxCover = document.querySelector('#addPhoto-cover');
+Dropzone.autoDiscover = false;
+
+const dropBoxCover = document.querySelector('#dropzone-cover');
 const addPhotoSvg = document.querySelector('#hide');
 
 const myDropzone = new Dropzone(dropBoxCover, {
-  url: '/file/post',
+  url:
+    'https://cors-anywhere.herokuapp.com/https://venify.herokuapp.com/user/photos/upload',
   maxFilesize: 10, // MB
-  maxFiles: 10,
+  maxFiles: 2,
+  paramName: 'file',
   dictDefaultMessage: '',
+  headers: {
+    authorization:
+      'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJwaG9uZV9udW1iZXIiOiI1NTU2NjIiLCJhZ2UiOjE1LCJpYXQiOjE1NzQ1MTMwMDh9.RDYu0nENanjZk8D-S1Ah9TuTrvKffO7l1h9ZWnRtlpAlGrOaf7gYVpkAI2NG2TFIBO80VtjNVihPxmUR01osOw',
+  },
   accept: function(file, done) {
     addPhotoSvg.classList.add('is-hidden');
-    if (!file.status === 'error') {
-      done();
-    } else if (myDropzone.files.length > 10) {
-      done('You can not upload any more files.');
-    } else {
-      done('Problems with downloading, image is not uploaded.');
-    }
+    done();
   },
+});
+
+myDropzone.on('complete', function(file) {
+  if (this.files.length === 2) {
+    console.log(file);
+  }
 });
