f2bf962c3f1fd45cff29e708340143c8108389ff
Sasha Savchuk <Sasha>
2019-11-24 15:51:42 +0200

    com

--- src/js/index.js
+++ src/js/index.js
@@ -5,13 +5,3 @@ import generatePeople from '../js/generatePeople'
 import axios from 'axios';
 mapGoogle()
 generatePeople()
-
-axios.post("https://venify.herokuapp.com/user/register",{
-	"password": "goit",
-	"login": "Lode_ssafastone111fffdsaafs",
-	"age": 15,
-	"phone_number": "555662",
-	"geo_location": [55.5555, 65.55555],
-	"gender": "mail"
-}
-).then(result => console.log(result)).catch(er => console.log(er  ))
